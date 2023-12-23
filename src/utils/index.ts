e50da0b92bfffc3605baa1c3efbf62570376cd53
YDH <swimmingpolar@DESKTOP-Soyun>
2023-12-23 21:36:57 +0900

    feat: performance check function

---
+++ src/utils/index.ts
@@ -0,0 +1,19 @@
+type Callback<Args extends any[], ReturnType> = (...args: Args) => ReturnType;
+
+export function measurePerformance<Args extends any[], ReturnType>(
+  callback: Callback<Args, ReturnType>
+) {
+  return function curried(...args: Args): ReturnType {
+    const start = performance.now();
+    const result = callback(...args);
+    const end = performance.now();
+
+    console.group("performance measure");
+    console.log("result: ");
+    console.log(result);
+    console.log("time: ", +end - start + "ms");
+    console.groupEnd();
+
+    return result;
+  };
+}
