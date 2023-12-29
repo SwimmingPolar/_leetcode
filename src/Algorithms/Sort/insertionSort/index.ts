6f0377198a616578ca800aa344cc3542bed35c67
SwimmingPolar <swimmingpolar@gmail.com>
2023-12-29 23:11:46 +0900

    Change insertion sort to readable code

--- src/Algorithms/Sort/insertionSort/index.ts
+++ src/Algorithms/Sort/insertionSort/index.ts
@@ -1,18 +1,24 @@
 function insertionSort(arr: number[]) {
+  // Iterate over the array starting from the second element
   for (let i = 1; i < arr.length; i++) {
-    let key = arr[i];
-    let j = i - 1;
+    // The current element to be inserted into the sorted portion
+    let currentElement = arr[i];
 
-    // Shift elements of arr[0..i-1], that are greater than key,
-    // to one position ahead of their current position
-    while (j >= 0 && arr[j] > key) {
+    // Find the position where this element should be inserted
+    // by comparing with elements in the sorted portion of the array
+    let j = i - 1;
+    while (j >= 0 && arr[j] > currentElement) {
+      // Shift the larger element to the right
       arr[j + 1] = arr[j];
-      j = j - 1;
+      j--;
     }
-    arr[j + 1] = key;
+
+    // Insert the current element into the found position
+    arr[j + 1] = currentElement;
   }
   return arr;
 }
 
 // Example usage
-console.log("Sorted array:", insertionSort([5, 2, 4, 6, 1, 3]));
+const array = [5, 2, 4, 6, 1, 3];
+console.log("Sorted array:", insertionSort(array));
